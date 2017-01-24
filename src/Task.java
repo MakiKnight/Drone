54c85e876e329e063500e80317170a13eeb7df67
Tirke <toj315@gmail.com>
2017-01-24 21:54:21 +0100

    Cleanup Task after enum refactor

--- src/Task.java
+++ src/Task.java
@@ -3,22 +3,11 @@
  */
 public class Task {
 
-    public final static int RECHARGE_DRONE = 1;
-    public final static int RECHARGE_BASE = 2;
-    public final static int LADE = 3;
-    public final static int DUMP = 4;
-    public final static int MOVE = 5;
+
     private final TaskTypes type;
     private Dot location;
     private Drone drone;
-    /**
-     * @param type     : 1 = recharge a Drone
-     *                 2 = recharge at the base
-     *                 3 = lade a packet
-     *                 4 = dump a packet
-     *                 5 = move somewhere
-     * @param location : where the task need to be execute
-     */
+
     public Task(TaskTypes type, Dot location, Drone drone) {
         this.type = type;
         this.location = location;
